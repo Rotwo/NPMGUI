a4756d8812f654f2ca9cd98d1be554b083052937
Rotwo <rotwodeveloper@gmail.com>
2025-09-09 21:49:29 +0200

    Create startup handler and add Dependency injection

--- NPMGUI.Core/Main.cs
+++ NPMGUI.Core/Main.cs
@@ -2,6 +2,8 @@
 using NPMGUI.Core.Models;
 using System.Diagnostics;
 using System.IO;
+using NPMGUI.Core.Services.ConfigLoader;
+using NPMGUI.Core.Services.PackageManagement;
 using NPMGUI.Core.Setup;
 
 namespace NPMGUI.Core
@@ -9,18 +11,23 @@ namespace NPMGUI.Core
     public class NPMGUICore
     {
         private string? _workDir;
+        private IConfigLoader _configLoader;
+        private IPackageManager _packageManager;
 
         public bool IsReady => IsValidWorkDir(_workDir);
 
-        public NPMGUICore(string? workDir)
+        public NPMGUICore(string? workDir, IConfigLoader configLoader, IPackageManager packageManager)
         {
+            _configLoader = configLoader;
+            _packageManager = packageManager;
+            
             if (!IsValidWorkDir(workDir))
             {
                 throw new InvalidOperationException("Invalid work directory");
             }
             
             _workDir = workDir;
-            SetupManager.Initialize(workDir!);
+            _configLoader.Load(workDir!);
         }
 
         private static bool IsValidWorkDir(string? workDir) => workDir != null && Path.Exists(workDir) && File.Exists(Path.Combine(workDir, "package.json"));
---
+++ NPMGUI.Core/Setup/CoreStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NPMGUI.Core.Services.ConfigLoader;
+using NPMGUI.Core.Services.PackageManagement;
+using NPMGUI.Core.Services.PackageService;
+
+namespace NPMGUI.Core.Setup;
+
+public static class CoreStartup
+{
+    public static ServiceProvider Initialize(string workDir)
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IConfigLoader, ConfigLoader>();
+        services.AddSingleton<IPackageManager, PackageManager>();
+        services.AddSingleton<IPackageService, PackageService>();
+
+        services.AddSingleton<NPMGUICore>();
+        
+        return services.BuildServiceProvider();
+    }
+}
