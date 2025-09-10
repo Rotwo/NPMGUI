fd5436c8d30751949fb2d44a19f13eac409a3a08
Rotwo <rotwodeveloper@gmail.com>
2025-09-10 16:47:17 +0200

    Temporaly removed config

--- NPMGUI.Core/Main.cs
+++ NPMGUI.Core/Main.cs
@@ -17,9 +17,7 @@ namespace NPMGUI.Core
         private readonly IConfigLoader _configLoader;
         private readonly IPackageService _packageService;
         private readonly PackageManagerFactory _factory;
-        
-        private IPackageManager _packageManager;
-        
+                
         public bool IsReady => IsValidWorkDir(_workDir);
 
         public NPMGUICore(IConfigLoader configLoader, IPackageService packageService, PackageManagerFactory factory)
@@ -35,12 +33,11 @@ namespace NPMGUI.Core
                 throw new InvalidOperationException("Invalid work directory");
 
             _workDir = workDir;
-            
-            _packageManager = _factory.Create(workDir!);
-            if (_packageManager is null)
-                throw new InvalidOperationException("No valid package manager found for this directory");
+        }
 
-            _configLoader.Load(workDir!);
+        public void Shutdown()
+        {
+            _configLoader.Save(_workDir!);
         }
 
         private static bool IsValidWorkDir(string? workDir) => workDir != null && Path.Exists(workDir) && File.Exists(Path.Combine(workDir, "package.json"));
