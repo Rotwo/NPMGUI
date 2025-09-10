8089ae9187ac7c73a5e0c04a22084f6f9b1fb7be
Rotwo <rotwodeveloper@gmail.com>
2025-09-10 16:48:18 +0200

    Fix indentation

--- NPMGUI.Core/Interfaces/IPackageService.cs
+++ NPMGUI.Core/Interfaces/IPackageService.cs
@@ -1,9 +1,11 @@
 ﻿using NPMGUI.Core.DTOs;
+using NPMGUI.Core.Interfaces;
+using NPMGUI.Core.Services.PackageManagement;
 
 namespace NPMGUI.Core.Services.PackageService;
 
 public interface IPackageService
 {
     PackageListing FindDependenciesOnDir(string workDir);
-   Task<TaskStatus> InstallPackage(string packageName, string packageVersion, string workDir, bool isDevDependency = false);
+    Task<TaskStatus> InstallPackage(string packageName, string packageVersion, string workDir, bool isDevDependency = false);
 }
